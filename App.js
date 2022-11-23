3b49e9e560fc054188034761c857ea1d12772999
Hansel Tanga Tita <99017156+softwarebyhans@users.noreply.github.com>
2022-11-23 15:27:02 +0700

    feat:  Remove back button from home screen

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <BackButton onButtonClick={updateScreen} />
+     { screen !== ScreenType.home && ( <BackButton onButtonClick={updateScreen} />)}
       {content}
     </View>
   )
